b2d73d1d0f53884a563517d836c8407e511245d6
akila199910 <akeeumayanga7775@gmail.com>
2023-09-05 09:53:49 +0530

    Update ReviewPage.php

--- Moderator/ReviewPage.php
+++ Moderator/ReviewPage.php
@@ -28,7 +28,7 @@ if(isset($_POST['view'])){
  
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-  <title>Review Page2</title>
+  <title>Review Page</title>
 </head>
 <body>
   <nav class="navbar navbar-light justify-content-center fs-3 mb-5" style="background-color: lightblue;width:100%;">
