3a60012782c0854331f533a8de871087f773ff09
akila199910 <akeeumayanga7775@gmail.com>
2023-09-09 13:15:59 +0530

    admin table
    admin table create

---
+++ Admin/Admin_dashboard/admintable.php
@@ -0,0 +1,13 @@
+<?php
+include "conn.php";
+mysqli_select_db($conn,"lplsystem");
+
+$sql2 = "CREATE TABLE IF NOT EXISTS admin (
+   first_name VARCHAR(255) NOT NULL,
+   last_name VARCHAR(255) NOT NULL,
+   email VARCHAR(255) NOT NULL,
+   password VARCHAR(255) NOT NULL,
+   profile_picture VARCHAR(255) NOT NULL)";
+
+   mysqli_query($conn, $sql2);
+?>
