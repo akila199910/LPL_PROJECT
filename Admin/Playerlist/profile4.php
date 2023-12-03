95c0b1d24a95f76252097baa0cfae726d5fd2e31
akila199910 <akeeumayanga7775@gmail.com>
2023-12-03 20:12:16 +0530

    photo upload paths
    interface view change

--- Admin/Playerlist/profile4.php
+++ Admin/Playerlist/profile4.php
@@ -96,6 +96,7 @@ if (isset($_SESSION['admin_id'])) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<title>Auction</title>
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
