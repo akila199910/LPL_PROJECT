f36c4859f57bb9a315ae27e5f69ead3eb421258f
akila199910 <akeeumayanga7775@gmail.com>
2023-12-04 22:45:03 +0530

    register error correct

--- Register/indexregister.php
+++ Register/indexregister.php
@@ -1,116 +1,5 @@
 
-<?php
-include("conn.php");
-mysqli_select_db($conn, "lplsystem");
 
-// Create playersregistation table if it doesn't exist
-
-$sql = "CREATE TABLE IF NOT EXISTS register (
-        player_id INT PRIMARY KEY AUTO_INCREMENT,
-        first_name VARCHAR(255) NOT NULL,
-        last_name VARCHAR(255) NOT NULL,
-        email VARCHAR(255) NOT NULL,
-        password VARCHAR(255) NOT NULL,
-        country VARCHAR(255) NOT NULL,
-        dob DATE NOT NULL,
-        profile_photo VARCHAR (255),
-        identity VARCHAR (255),
-        identity_number VARCHAR (255),
-        identity_photo VARCHAR (255),
-        catogary VARCHAR (255),
-        capped VARCHAR (255),
-        additional_details TEXT,
-        certificate_photo VARCHAR(255),
-        verification VARCHAR(255),
-        verification_code VARCHAR(255),
-        approved VARCHAR (255) NULL,
-        moderators_id INT
-
-    )";
-
-mysqli_query($conn, $sql);
-
-
-if (isset($_POST['submit'])) {
-    
-    
-    $email = $_POST['email'];
-    $check_email_query = "SELECT * FROM register WHERE email = '$email'";
-    $result = mysqli_query($conn, $check_email_query);
-
-    if (mysqli_num_rows($result) > 0) {
-        // Email already exists, show an error message or redirect to a registration page
-        echo "<script>alert('Email already exists. Please use a different email.');</script>";
-    } else {
-
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $country = $_POST['country'];
-    $dob = $_POST['dob'];
-    $identity = $_POST['identity'];
-    $identity_number = $_POST['identity_number'];
-    $catogary = $_POST['catogary'];
-    $capped = $_POST['capped'];
-    $additional_details = $_POST['additional_details'];
-    // Encrypt the password using password_hash() function
-$hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-
-
-    $file_name1=$_FILES['identity_photo']['name'];
-    $file_type1=$_FILES['identity_photo']['type'];
-    $file_size1=$_FILES['identity_photo']['size'];
-    $temp_name1=$_FILES['identity_photo']['tmp_name'];
-    $upload_to="Img/idimg/";
-    move_uploaded_file($temp_name1,$upload_to.$file_name1);
-
-    $file_name2=$_FILES['certificatephoto']['name'];
-    $file_type2=$_FILES['certificatephoto']['type'];
-    $file_size2=$_FILES['certificatephoto']['size'];
-    $temp_name2=$_FILES['certificatephoto']['tmp_name'];
-    $upload_to="Img/cetiimg/";
-    move_uploaded_file($temp_name2,$upload_to.$file_name2);  
-    //$certificate_photo=$_POST['certificate_photo'];
-
-
-    $file_name3=$_FILES['profile_photo']['name'];
-    $file_type3=$_FILES['profile_photo']['type'];
-    $file_size3=$_FILES['profile_photo']['size'];
-    $temp_name3=$_FILES['profile_photo']['tmp_name'];
-    $upload_to="Img/proimg/";
-    move_uploaded_file($temp_name3,$upload_to.$file_name3); 
-    //$identity_photo = $_POST['identity_photo'];
-    //$certificate_photo = $_POST['certificate_photo'];
-    //$profile_photo = $_POST['profile_photo'];
-    
-  // Generate a verification code (you can use a stronger method if needed)
-  $verif_code = bin2hex(random_bytes(16));
-
-  // Insert the user data into the database with the verification code
-  $sql3 = "INSERT INTO register (first_name, last_name, email, password, country, dob, identity, identity_number, catogary, capped, additional_details, profile_photo, identity_photo, certificate_photo,verification, verification_code)
-  VALUES ('$first_name','$last_name','$email','$hashed_password','$country','$dob','$identity','$identity_number','$catogary','$capped','$additional_details','$file_name3','$file_name1','$file_name2', 0, '$verif_code')";
-
-  if (mysqli_query($conn, $sql3)) {
-      // Send a verification email
-      $subject = "Verify Your Email Address";
-      $message = "Click the following link to verify your email address: http://localhost/LPL_PROJECT/LPL_PROJECT/Register/verify.php?code=$verif_code";
-      $headers = "From: premierleaguesrilanka@gmail.com";
-      
-      mail($email, $subject, $message, $headers);
-      echo "<script>alert('Check your mail box');</script>";
-
-    }
-
-    
-    echo '<script> window.location.href = "http://localhost/LPL_PROJECT/LPL_PROJECT/loginform.php"</script>';
-   
-    
-}
-}
-
-?>
 
 
 
@@ -300,44 +189,136 @@ $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
 </html>
 
-
 <?php
+include("conn.php");
+mysqli_select_db($conn, "lplsystem");
 
-if($query){
-    ?>
-  <script>
-swal({
-    title: "SUCCESS..!!",
-    text: "Verify Your Email Now..!!",
-    icon: "success",
-    buttons: {
-        goodJob: {
-            text: "GOOD JOB",
-            value: "goodJob",
+// Create playersregistation table if it doesn't exist
+
+$sql = "CREATE TABLE IF NOT EXISTS register (
+        player_id INT PRIMARY KEY AUTO_INCREMENT,
+        first_name VARCHAR(255) NOT NULL,
+        last_name VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        country VARCHAR(255) NOT NULL,
+        dob DATE NOT NULL,
+        profile_photo VARCHAR (255),
+        identity VARCHAR (255),
+        identity_number VARCHAR (255),
+        identity_photo VARCHAR (255),
+        catogary VARCHAR (255),
+        capped VARCHAR (255),
+        additional_details TEXT,
+        certificate_photo VARCHAR(255),
+        verification VARCHAR(255),
+        verification_code VARCHAR(255),
+        approved VARCHAR (255) NULL,
+        moderators_id INT
+
+    )";
+
+mysqli_query($conn, $sql);
+
+
+if (isset($_POST['submit'])) {
+    
+    
+    $email = $_POST['email'];
+    $check_email_query = "SELECT * FROM register WHERE email = '$email'";
+    $result = mysqli_query($conn, $check_email_query);
+
+    if (mysqli_num_rows($result) > 0) {
+        // Email already exists, show an error message or redirect to a registration page
+        echo "<script>alert('Email already exists. Please use a different email.');</script>";
+    } else {
+
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $country = $_POST['country'];
+    $dob = $_POST['dob'];
+    $identity = $_POST['identity'];
+    $identity_number = $_POST['identity_number'];
+    $catogary = $_POST['catogary'];
+    $capped = $_POST['capped'];
+    $additional_details = $_POST['additional_details'];
+    // Encrypt the password using password_hash() function
+$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+
+
+    $file_name1=$_FILES['identity_photo']['name'];
+    $file_type1=$_FILES['identity_photo']['type'];
+    $file_size1=$_FILES['identity_photo']['size'];
+    $temp_name1=$_FILES['identity_photo']['tmp_name'];
+    $upload_to="Img/idimg/";
+    move_uploaded_file($temp_name1,$upload_to.$file_name1);
+
+    $file_name2=$_FILES['certificatephoto']['name'];
+    $file_type2=$_FILES['certificatephoto']['type'];
+    $file_size2=$_FILES['certificatephoto']['size'];
+    $temp_name2=$_FILES['certificatephoto']['tmp_name'];
+    $upload_to="Img/cetiimg/";
+    move_uploaded_file($temp_name2,$upload_to.$file_name2);  
+    //$certificate_photo=$_POST['certificate_photo'];
+
+
+    $file_name3=$_FILES['profile_photo']['name'];
+    $file_type3=$_FILES['profile_photo']['type'];
+    $file_size3=$_FILES['profile_photo']['size'];
+    $temp_name3=$_FILES['profile_photo']['tmp_name'];
+    $upload_to="Img/proimg/";
+    move_uploaded_file($temp_name3,$upload_to.$file_name3); 
+    //$identity_photo = $_POST['identity_photo'];
+    //$certificate_photo = $_POST['certificate_photo'];
+    //$profile_photo = $_POST['profile_photo'];
+    
+  // Generate a verification code (you can use a stronger method if needed)
+  $verif_code = bin2hex(random_bytes(16));
+
+  // Insert the user data into the database with the verification code
+  $sql3 = "INSERT INTO register (first_name, last_name, email, password, country, dob, identity, identity_number, catogary, capped, additional_details, profile_photo, identity_photo, certificate_photo,verification, verification_code)
+  VALUES ('$first_name','$last_name','$email','$hashed_password','$country','$dob','$identity','$identity_number','$catogary','$capped','$additional_details','$file_name3','$file_name1','$file_name2', 0, '$verif_code')";
+
+    $query=mysqli_query($conn, $sql3);
+
+    if($query){
+        ?>
+      <script>
+    swal({
+        title: "SUCCESS..!!",
+        text: "Verify Your Email Now..!!",
+        icon: "success",
+        buttons: {
+            goodJob: {
+                text: "GOOD JOB",
+                value: "goodJob",
+            },
         },
-    },
-}).then((value) => {
-    if (value === "goodJob") {
-        // Add your web address here
-        window.location.href = "http://localhost/LPL_PROJECT/LPL_PROJECT/loginform.php";
+    }).then((value) => {
+        if (value === "goodJob") {
+            // Add your web address here
+            window.location.href = "http://localhost/LPL_PROJECT/LPL_PROJECT/loginform.php";
+        }
+    });
+    </script>
+    <?php
     }
-});
-</script>
-<?php
-}
-
-if ($query) {
-
-  // Send a verification email
- $subject = "Verify Your Email Address";
-  $message = "Click the following link to verify your email address: http://localhost/LPL_PROJECT/LPL_PROJECT/Register/verify.php?code=$verif_code";
-  $headers = "From: premierleaguesrilanka@gmail.com";
-  
-  mail($email, $subject, $message, $headers);
-  }
-
-// echo "<script>alert('Check your mail box');</script>";
-//  echo '<script> window.location.href = "http://localhost/LPL_PROJECT/LPL_PROJECT/loginform.php"</script>';
-
-mysqli_close($conn);
-?>
+    
+    if ($query) {
+    
+      // Send a verification email
+     $subject = "Verify Your Email Address";
+      $message = "Click the following link to verify your email address: http://localhost/LPL_PROJECT/LPL_PROJECT/Register/verify.php?code=$verif_code";
+      $headers = "From: premierleaguesrilanka@gmail.com";
+      
+      mail($email, $subject, $message, $headers);
+      }
+    
+    // echo "<script>alert('Check your mail box');</script>";
+    //  echo '<script> window.location.href = "http://localhost/LPL_PROJECT/LPL_PROJECT/loginform.php"</script>';
+    }}
+    mysqli_close($conn);
+    ?>
