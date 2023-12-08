8f2ec60bc1bebfb00297e2a33cc070364ce3dfc3
dilanka16 <135750969+dilanka16@users.noreply.github.com>
2023-12-08 15:53:50 +0530

    register style

--- Admin/Playerlist/registered.php
+++ Admin/Playerlist/registered.php
@@ -46,6 +46,71 @@ if (isset($_SESSION['admin_id'])) {
       text-align: center;
     }
 
+
+    .size {
+              width: 200px;
+              height: 200px;
+          }
+          .btn-margin {
+            margin-bottom: 15px;
+        }
+
+
+        .navbar{
+    display: flex;
+    align-items: center;
+    padding: 20px;
+}
+
+nav{
+    flex: 1;
+    text-align: right;
+}
+
+nav ul{
+    display: inline-block;
+    list-style-type: none;
+}
+
+nav ul li{
+    display: inline-block;
+    margin-right: 20px;
+}
+
+nav ul li i{
+    margin-right: 15px;
+
+}
+
+a{
+    text-decoration: none;
+    color: #555;
+}
+
+.header{
+    background: radial-gradient(#fff,#5960de);
+    height: 400vh;
+}
+
+
+.card{
+    width: 80%;
+    max-width: 3000px;
+    color: #fff;
+    text-align: center;
+    padding: 50px -100px;
+    border: 1px solid rgba(255,255,255,0.3);
+    background: rgba(255,255,255,0.2);
+    border-radius: 16px;
+    box-shadow: 0 4px 30px rgba(0,0,0,0.1);
+    backdrop-filter: blur(5px);
+    margin-left: auto;
+    margin-right: 0px;
+
+}
+
+
+
   </style>
 </head>
 <body>
@@ -53,14 +118,25 @@ if (isset($_SESSION['admin_id'])) {
 
 include('../sidebar.php');
 ?>
-  <nav class="navbar navbar-light justify-content-center fs-3 mb-5" style="background-color: lightblue;width:100%;">
-    LPL - LANKA PREMIER LEAGUE
-  </nav>
+  <div class="header">
+<div class="navbar">
+<img src="/LPL_PROJECT/LPL_PROJECT/images/lpllogo.png" width="125px" >
+
+  <nav>
+<ul>
+    <li><i class="fa-solid fa-laptop"></i><a href="">Dashboard</a></li>
+    <li><i class="fa-solid fa-right-from-bracket"></i><a href="">Log Out</a></li>
+</ul>
+</nav>
+
+</div>
+<div class="card" data-tilt>
   <div class="content">
 
 
   <div class="container">
     <table class="table table-hover text-center">
+      
       <thead>
         <tr>
        
@@ -106,6 +182,8 @@ include('../sidebar.php');
 ?>
       </tbody>
     </table>
+    </div>
+    
   
 </body>
 </html>
