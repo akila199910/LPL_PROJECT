e5250ab7301b183375904c54352eb65166d641fe
akila199910 <akeeumayanga7775@gmail.com>
2023-12-11 16:30:14 +0530

    Update ReviewPage.php

--- Moderator/ReviewPage.php
+++ Moderator/ReviewPage.php
@@ -106,6 +106,20 @@ a{
     margin-right: auto;
 
 }
+
+.btn{
+    display: inline-block;
+    background: #ff523b;
+    
+    padding: 8px 30px;
+    margin: -40px 0;
+    border-radius: 30px;
+    transition: background 0.5s;
+}
+
+.btn:hover{
+    background: #5960de;
+}
   
   
   </style>
