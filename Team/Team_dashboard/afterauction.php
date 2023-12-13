6d818bb913b7e9ffaf90942a883a22f12072ff6f
dilanlahiru97 <ddlwijayasekara@gmail.com>
2023-12-13 19:10:01 +0530

    After auction page create

--- Team/Team_dashboard/afterauction.php
+++ Team/Team_dashboard/afterauction.php
@@ -13,8 +13,8 @@ if ($result && mysqli_num_rows($result) > 0) {
     $row = mysqli_fetch_assoc($result);
 
     $player_id= $row["player_id"];
-    echo $player_id."<br>";
-
+   
+   
     
 }
         $Catogary= "SELECT catogary,first_name,last_name,profile_photo FROM register WHERE player_id=$player_id";
@@ -34,6 +34,9 @@ if ($result && mysqli_num_rows($result) > 0) {
             
     }
 
+  
+
+
     $bidDetails = "SELECT team_id, bid_price FROM bid WHERE player_id = $player_id ORDER BY bid_id DESC LIMIT 1";
 
     $BidRsult=mysqli_query($conn, $bidDetails);
@@ -51,13 +54,47 @@ if ($result && mysqli_num_rows($result) > 0) {
         $team_id="No bid for You";
     }
 
+    if ($bid_price > 0) {
+        echo '
+        <div class="winer"><div class="win-container">
+            <div class="box">
+                <h2>Congratulations!</h2>
+                <h3>You have won the bid</h3>';
+                echo "Player ID ".$player_id."<br><br><h3>";
+                echo $first_name." ".$last_name."</h3><br> <br>";
+                echo '<img src="../../Register/Img/proimg/' . $profile_photo . '" alt="Profile Picture" width="150px" height="150px">';
+               
     
-echo "$team_id"."<br>";
-echo"$PlayerCatogary"."<br>";
-echo $first_name." ".$last_name."<br>";
-echo "$profile_photo"."<br>";
-echo $bid_price."<br>";
+                echo '<p>Team ID: ' . $team_id . '</p>
+                <p style="font-size: 25px;">' . $PlayerCatogary . '</p>
+                <p><b>Bid Price: ' . $bid_price . '<b></p>
+            </div>
+        </div>
+        <div>';
+    }
     
+
+    
+
+
+    else {
+        echo '<div class="lost-container">
+                <div class="box">
+                    Player ID ' . $player_id . '<br><h3>' .
+                    $first_name . ' ' . $last_name . '</h3><br><br>' .
+                    '<img src="../../Register/Img/proimg/' . $profile_photo . '" alt="Profile Picture" width="150px" height="150px">
+                    <p style="font-size: 25px;">' . $PlayerCatogary . '</p>
+                    <h4>Sorry <br>you are not sold</h4>
+                    <img src="Sad.png" alt="Girl in a jacket" width="60" height="60"> 
+                    <p style="font-size: 30px;">Try again next round.</p>
+                </div>
+            </div>';
+    }
+    
+     
+
+    
+
 
 ?>
 
@@ -96,8 +133,65 @@ echo $bid_price."<br>";
 </script>
 
 
+<style>
+
+h2 {
+	
+	font-size: 50px;
+	font-family: 'Sigmar One', cursive;
+	
+}
+
+h4 {
+	
+	font-size: 50px;
+	font-family: 'Sigmar One', cursive;
+	
+}
+
+
+.win-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-image: url('win.gif'); 
+    background-size: cover; /* Ensuring the image covers the container */
+    background-position: center; /* Centering the image */
+}
+
+
+
+.box {
+    text-align: center;
+    padding: 20px;
+    background-color: rgba(255, 255, 245, 0.8); /* Adding a semi-transparent background color */
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+    max-width: 80%;
+}
+
+
+.lost-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-color: #c7e0de; 
+    background-size: cover; /* Ensuring the image covers the container */
+    background-position: center; /* Centering the image */
+}
+
+.winer{
+    background-color: #c7e0de; 
+
+}
+
+
+
+</style>
+
+
 </head>
-<body>
-    <!-- Your page content goes here -->
-</body>
+
 </html>
