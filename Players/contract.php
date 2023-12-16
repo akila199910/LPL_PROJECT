b30f1a462192d3242c232bdacb314b00ad9b0985
dilanka16 <135750969+dilanka16@users.noreply.github.com>
2023-12-16 18:03:57 +0530

    Update contract.php

--- Players/contract.php
+++ Players/contract.php
@@ -54,20 +54,114 @@ if ($result->num_rows > 0) {
             margin-top: 10px;
           }
 
+
+          .navbar {
+    display: flex;
+    align-items: center;
+    padding: 20px;
+    background-color: #4169E1;
+    z-index: 1000;
+    width: 100vw; 
+    position: fixed; 
+    top: 0; 
+    left: 0; 
+}
+
+
+nav{
+    flex: 1;
+    text-align: right;
+}
+
+nav ul{
+    display: inline-block;
+    list-style-type: none;
+}
+
+nav ul li{
+    display: inline-block;
+    margin-right: 20px;
+}
+
+nav ul li i{
+    margin-right: 15px;
+
+}
+
+a{
+    text-decoration: none;
+    color: #555;
+}
+
+p{
+    color: #fff;
+    text-align:center;
+}
+
+.header{
+    background: radial-gradient(#fff,#5960de);
+    height: 500vh;
+}
+
+.card{
+    width: 60%;
+    max-width: 2000px;
+    color: #000;
+    text-align: center;
+    padding: 50px 35px;
+    border: 1px solid rgba(255,255,255,0.3);
+    background: rgba(255,255,255,0.2);
+    border-radius: 16px;
+    box-shadow: 0 4px 30px rgba(0,0,0,0.1);
+    backdrop-filter: blur(5px);
+    margin-left: auto;
+    margin-right: auto;
+
+}
+
+
+.btn{
+    display: inline-block;
+    background: #ff523b;
+    
+    padding: 8px 30px;
+    
+    border-radius: 30px;
+    transition: background 0.5s;
+}
+
+.btn:hover{
+    background: #5960de;
+}
+
+h2{
+  text-align:center;
+  text-decoration: underline;
+}
         </style>
 </head>
+<div class="header">
 <body>
-<nav class="navbar navbar-light justify-content-center fs-3 mb-5" style="background-color: lightblue;width:100%;">
-  LPL - LANKA PREMIER LEAGUE
-</nav>  
+<div class="navbar row">
+        <div class="logo col-4" >
+           <img src="../images/lpllogo.png" width="125px"> 
+        </div>
 
+        <div class="col-8" style="color: #fff; font-size:20px;">   LPL - LANKA PREMIER LEAGUE</div>
+        </nav>
+       
+    </div>
+    <br><br><br><br><br><br> 
+    <h2>If you already contracted with any team please fill this.</h2>
+    <br>
+    <div class="card" data-tilt>
 <div class="content">
   <div class="container">
     <div class="row">
     <div class="row justify-content-center">
-      <div class="col-lg-6">
-      <form action="" class="form-box" method="POST" enctype="multipart/form-data">
-        <p>If you already contracted with any team please fill this.</p>
+      <div>
+      <form action=""  method="POST" enctype="multipart/form-data">
+       
         <div class="form-group col-6">
           <select name="identity" class="form-select" required>
             <option value="" disabled selected hidden>Select your team here</option>
@@ -84,7 +178,7 @@ if ($result->num_rows > 0) {
         </div>
         <div class="col-lg-6 justify-content-center">
         
-          <button type="submit" name="submit" id="submit" class="btn btn-primary btn-sm" >SUBMIT</button>
+          <button type="submit" name="submit" id="submit" class="btn" >SUBMIT</button>
         </div>
         </form>
       </div>
