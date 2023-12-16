629d599e41ad3266b41423a37a82ce74a9a33934
dilanka16 <135750969+dilanka16@users.noreply.github.com>
2023-12-16 11:24:38 +0530

    style

--- Admin/Rule/updaterule.php
+++ Admin/Rule/updaterule.php
@@ -73,10 +73,136 @@ if (isset($_SESSION['admin_id'])) {
 
     <!-- Main CSS-->
     <link href="css/main.css" rel="stylesheet" media="all">
+    <style>
+        
+        td {
+      vertical-align: middle;
+    }
+
+    .text {
+      text-align: center;
+    }
+
+
+    .size {
+      width: 200px;
+      height: 200px;
+    }
+
+    .btn-margin {
+      margin-bottom: 15px;
+    }
+
+
+
+
+    .header {
+      background: radial-gradient(#fff, #5960de);
+      height: 100%;
+    }
+
+
+    .card {
+      width: 80%;
+      max-width: 3000px;
+      color: #fff;
+      text-align: center;
+      padding: 50px -100px;
+      border: 1px solid rgba(255, 255, 255, 0.3);
+      background: rgba(255, 255, 255, 0.2);
+      border-radius: 16px;
+      box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+      backdrop-filter: blur(5px);
+      margin-left: auto;
+      margin-right: 30px;
+
+    }
+
+.navbar {
+    display: flex;
+    align-items: center;
+    padding: 20px;
+    background-color: #4169E1;
+    z-index: 1000;
+    width: 100vw; 
+    position: fixed; 
+    top: 0; 
+    left: 0; 
+}
+
+
+nav{
+    flex: 1;
+    text-align: right;
+}
+    .navbar {
+      display: flex;
+      align-items: center;
+      padding: 20px;
+      background-color: #4169E1;
+      justify-content: space-between;
+    align-items: center;
+    }
+
+    nav {
+      flex: 1;
+      text-align: right;
+    }
+
+    nav ul {
+      display: inline-block;
+      list-style-type: none;
+    }
+
+    nav ul li {
+      display: inline-block;
+      margin-right: 20px;
+    }
+
+    nav ul li i {
+      margin-right: 15px;
+
+    }
+
+    a {
+      text-decoration: none;
+      color: #555;
+    }
+
+    p {
+      color: #fff;
+      text-align: center;
+    }
+
+    .col-8 {
+    text-align: center;
+    margin-right: 600px;
+  }
+        </style>
+       
 </head>
+<div class="header">
 
 <body>
-    <div class="page-wrapper bg-gra-03 p-t-45 p-b-50">
+<?php
+
+//include('/LPL_PROJECT/LPL_PROJECT/Admin/sidebar.php');
+include('sidebar.php');
+
+?>
+
+<div class="navbar row">
+      <div class="logo col-4">
+        <img src="../../images/lpllogo.png" width="125px">
+      </div>
+
+      <div class="col-8" style="color: #fff; font-size:20px; text-align: center;"> LPL - LANKA PREMIER LEAGUE</div>
+      </nav>
+
+    </div>
+    <br><br><br><br><br><br>
+  
+    
         <div class="wrapper wrapper--w790">
             <div class="card card-5">
                 <div class="card-heading">
@@ -138,6 +264,14 @@ if (isset($_SESSION['admin_id'])) {
                             <div class="value">
                                 <div class="input-group">
                                     <input class="input--style-5" type="number" name="register_period">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="name">Auction Start Date</div>
+                            <div class="value">
+                                <div class="input-group">
+                                    <input class="input--style-5" type="date" name="start" value="<?php echo $start?>">
                                 </div>
                             </div>
                         </div>
