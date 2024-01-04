30f8d2fe64700c119be8482f1e7e06f9b74dc613
dilanlahiru97 <ddlwijayasekara@gmail.com>
2024-01-04 14:58:08 +0530

    Update password_reset_handler.php

--- Login/forgot_password/password_reset_handler.php
+++ Login/forgot_password/password_reset_handler.php
@@ -14,7 +14,7 @@ if(isset($_POST['reset_password'])){
         $sql_reset_password_register = "UPDATE register SET password = '$hashed_password', verification_code = NULL WHERE verification_code = '$token'";
         $result_register = $conn->query($sql_reset_password_register);
         
-        else
+        
         // Update password in the guest table if verification code matches
         $sql_reset_password_guest = "UPDATE guest SET password = '$hashed_password', verification_code = NULL WHERE verification_code = '$token'";
         $result_guest = $conn->query($sql_reset_password_guest);
